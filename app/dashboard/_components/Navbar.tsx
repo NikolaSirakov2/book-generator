7f169aaaacb52a89c8cb7f3e9891e08c14925cfc
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-08-01 22:36:26 +0300

    refactor: Update Navbar component to use DropdownMenu for improved mobile navigation

--- app/dashboard/_components/Navbar.tsx
+++ app/dashboard/_components/Navbar.tsx
@@ -32,77 +32,92 @@ function NavBar() {
       <div className="lg:hidden flex justify-between items-center bg-gradient-to-br from-blue-200 via-blue-500 to-blue-200">
         <Image src="/InkastleLogo.png" alt="logo" width={70} height={30} />
         <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" className="mr-2 my-2">
-            Menu
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="flex justify-center items-center w-screen h-screen text-white bg-gray-400">
-          <DropdownMenuGroup>
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard">
-                <span
-                  className={`flex items-center ${
-                    isActive("/dashboard") ? "text-black" : ""
-                  }`}
-                >
-                  <HomeIcon className="mr-2 h-8 w-8" />
-                  <span className="text-2xl">Home</span>
-                </span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard/mybooks">
-                <span
-                  className={`flex items-center ${
-                    isActive("/dashboard/mybooks") ? "text-black" : ""
-                  }`}
-                >
-                  <LibraryBig className="mr-2 h-8 w-8" />
-                  <span className="text-2xl">My Books</span>
-                </span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard/createbook">
-                <span
-                  className={`flex items-center ${
-                    isActive("/dashboard/createbook") ? "text-black" : ""
-                  }`}
-                >
-                  <BookPlus className="mr-2 h-8 w-8" />
-                  <span className="text-2xl">Create Book</span>
-                </span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard/subscriptions">
-                <span
-                  className={`flex items-center ${
-                    isActive("/dashboard/subscriptions") ? "text-black" : ""
-                  }`}
-                >
-                  <CircleDollarSign className="mr-2 h-8 w-8" />
-                  <span className="text-2xl">Subscriptions</span>
-                </span>
-              </Link>
-            </DropdownMenuItem>
-    
-            <DropdownMenuItem asChild>
-              <Link href="/login">
-                <span
-                  className={`flex items-center ${
-                    isActive("/user/login") ? "text-black" : ""
-                  }`}
-                >
-                  <LogOut className="mr-2 h-8 w-8" />
-                  <span className="text-2xl">Log out</span>
-                </span>
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="mr-2 my-2">
+              Menu
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="flex justify-center items-center w-screen h-screen text-white bg-gray-400">
+            <DropdownMenuGroup>
+              <DropdownMenuItem asChild>
+                <Link href="/dashboard">
+                  <span
+                    className={`flex items-center ${
+                      isActive("/dashboard") ? "text-black" : ""
+                    }`}
+                  >
+                    <HomeIcon className="mr-2 h-8 w-8" />
+                    <span className="text-2xl">Home</span>
+                  </span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/dashboard/mybooks">
+                  <span
+                    className={`flex items-center ${
+                      isActive("/dashboard/mybooks") ? "text-black" : ""
+                    }`}
+                  >
+                    <LibraryBig className="mr-2 h-8 w-8" />
+                    <span className="text-2xl">My Books</span>
+                  </span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/dashboard/createbook">
+                  <span
+                    className={`flex items-center ${
+                      isActive("/dashboard/createbook") ? "text-black" : ""
+                    }`}
+                  >
+                    <BookPlus className="mr-2 h-8 w-8" />
+                    <span className="text-2xl">Create Book</span>
+                  </span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href="/dashboard/subscriptions">
+                  <span
+                    className={`flex items-center ${
+                      isActive("/dashboard/subscriptions") ? "text-black" : ""
+                    }`}
+                  >
+                    <CircleDollarSign className="mr-2 h-8 w-8" />
+                    <span className="text-2xl">Subscriptions</span>
+                  </span>
+                </Link>
+              </DropdownMenuItem>
+              {decodedToken && decodedToken.id && (
+                <DropdownMenuItem asChild>
+                  <Link href={`/dashboard/account/${decodedToken.id}`}>
+                    <span
+                      className={`flex items-center ${
+                        isActive(`/dashboard/account/${decodedToken.id}`)
+                          ? "text-black"
+                          : ""
+                      }`}
+                    >
+                      <Settings className="mr-2 h-8 w-8" />
+                      <span className="text-2xl">Account</span>
+                    </span>
+                  </Link>
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuItem asChild>
+                <Link href="/login">
+                  <span
+                    className={`flex items-center ${
+                      isActive("/user/login") ? "text-black" : ""
+                    }`}
+                  >
+                    <LogOut className="mr-2 h-8 w-8" />
+                    <span className="text-2xl">Log out</span>
+                  </span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <BreadcrumbComponent />
     </div>
