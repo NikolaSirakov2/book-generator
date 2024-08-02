5920faac4af4459e34de5f710ac38e310a9cd1b5
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-08-02 11:36:04 +0300

    Add Capacitor for mobile exports

--- app/dashboard/account/[id]/page.tsx
+++ app/dashboard/account/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useBookGenerationSocket } from "../../_components/BookGenerationSocket";
@@ -36,6 +36,7 @@ function Account({ params }: { params: { id: string } }) {
     return decodedJwt;
   };
   const user = getDecodedToken();
+  console.error(user);
 
   const handleClick = (event: React.MouseEvent) => {
     const emailRegex = /\S+@\S+\.\S+/;
@@ -54,53 +55,47 @@ function Account({ params }: { params: { id: string } }) {
 
   function handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
-    
   }
 
   return (
     <div className="flex items-center justify-center">
-        <div className="flex flex-col mt-5 lg:mt-20 w-full h-[80vh] lg:w-[50vw] border rounded-xl shadow-xl bg-gradient-to-br from-blue-200 via-blue-300 to-blue-200">
-          <h1 className="text-4xl font-bold text-center my-8">User details</h1>
-          <h2 className="ml-4 font-bold">First Name</h2>
-          <input
-            type="text"
-            placeholder="enter title of your story"
-            value={user.firstName}
-            readOnly
-            className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
-          />
-          <h2 className="ml-4 font-bold">Last Name</h2>
-          <input
-            type="text"
-            placeholder="enter number of pages"
-            value={user.lastName}
-            readOnly
-            className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
-          />
-          <h2 className="ml-4 font-bold">Email</h2>
-          <input
-            type="text"
-            placeholder="enter number of pages"
-            value={user.email}
-            readOnly
-            className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
-          />
-          <div className="flex items-center justify-center gap-6">
-            <Button
-              onClick={handleSubmit}
-              className="mt-10 mb-6 w-1/3 bg-black hover:bg-white hover:text-black"
-            >
-              Save changes
-            </Button>
-            <Link href="/">
-            <Button
-              className="mt-10 mb-6 w-[15vw] bg-red-400 hover:bg-white hover:text-red-400"
-            >
+      <div className="flex flex-col mt-5 lg:mt-20 w-full h-[80vh] lg:w-[50vw] border rounded-xl shadow-xl bg-gradient-to-br from-blue-200 via-blue-300 to-blue-200">
+        <h1 className="text-4xl font-bold text-center my-8">User details</h1>
+        <h2 className="ml-4 font-bold">First Name</h2>
+        <input
+          type="text"
+          placeholder="enter title of your story"
+          value={user.firstName}
+          className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
+        />
+        <h2 className="ml-4 font-bold">Last Name</h2>
+        <input
+          type="text"
+          placeholder="enter number of pages"
+          value={user.lastName}
+          className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
+        />
+        <h2 className="ml-4 font-bold">Email</h2>
+        <input
+          type="text"
+          placeholder="enter number of pages"
+          value={user.email}
+          className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
+        />
+        <div className="flex items-center justify-center gap-6">
+          <Button
+            onClick={handleSubmit}
+            className="mt-10 mb-6 w-1/3 bg-black hover:bg-white hover:text-black"
+          >
+            Save changes
+          </Button>
+          <Link href="/">
+            <Button className="mt-10 mb-6 w-[15vw] bg-red-400 hover:bg-white hover:text-red-400">
               Logout
             </Button>
-            </Link>
-          </div>
+          </Link>
         </div>
+      </div>
     </div>
   );
 }
