0283aa45e3acf7e5a6a0b15123e65e2efef5d4c1
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-08-03 19:47:26 +0300

    refactor: Update Account page to handle user information updates
    The Account page has been refactored to handle user information updates. This update includes adding state variables for password confirmation, first name, last name, and initial values. The useEffect hook is used to populate the form fields with the user's existing information. The handleSubmit function has been modified to check for changes before submitting the form and display a toast message if no changes are detected. Additionally, the changeUser API function has been updated to send a PUT request with the updated user information. This refactor improves the functionality and user experience of the Account page.

--- app/dashboard/account/[id]/page.tsx
+++ app/dashboard/account/[id]/page.tsx
@@ -1,22 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useBookGenerationSocket } from "../../_components/BookGenerationSocket";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { changeUser } from "@/app/api/api";
+import { changeUser } from '../../../api/api';
+import { useToast } from "@/components/ui/use-toast";
 
-function Account({ params }: { params: { id: string } }) {
+function Account({ }: { params: { id: string } }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [initialValues, setInitialValues] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+  });
   const [error, setError] = useState({
     email: false,
     password: false,
   });
   let router = useRouter();
+  const { toast } = useToast();
   useBookGenerationSocket();
 
-  const token = localStorage.getItem("token") ?? "";
+  const isBrowser = typeof window !== "undefined";
+  const token = isBrowser ? localStorage.getItem("token") ?? "" : "";
   const getDecodedToken = () => {
     let decodedJwt = null;
 
@@ -39,23 +50,54 @@ function Account({ params }: { params: { id: string } }) {
   const user = getDecodedToken();
   console.error(user);
 
-  const handleClick = (event: React.MouseEvent) => {
-    const emailRegex = /\S+@\S+\.\S+/;
-    let errors = { email: false, password: false };
-    if (!emailRegex.test(email)) {
-      errors.email = true;
+  useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName || "");
+      setLastName(user.lastName || "");
+      setEmail(user.email || "");
+      setInitialValues({
+        email: user.email || "",
+        firstName: user.firstName || "",
+        lastName: user.lastName || "",
+      });
     }
-    if (password === "") {
-      errors.password = true;
-    }
-    if (errors.email || errors.password) {
-      event.preventDefault();
-    }
-    setError(errors);
+  }, []); // Empty dependency array to run only once on mount
+
+  const hasChanges = () => {
+    return (
+      email !== initialValues.email ||
+      firstName !== initialValues.firstName ||
+      lastName !== initialValues.lastName ||
+      password !== "" ||
+      passwordConfirmation !== ""
+    );
   };
 
-  function handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
+  async function handleSubmit(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
+    if (!hasChanges()) {
+      toast({
+        title: "No changes detected",
+        description: "Please make some changes before saving.",
+      });
+      return;
+    }
+    try {
+      const userInfo = {
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordConfirmation,
+      };
+      const updatedUser = await changeUser(user, token, userInfo);
+      console.error("User updated successfully:", updatedUser, userInfo);
+      toast({
+        title: `Your changes have been saved!`, 
+      });
+    } catch (error) {
+      console.error("Error updating user:", error);
+    }
   }
 
   return (
@@ -65,22 +107,41 @@ function Account({ params }: { params: { id: string } }) {
         <h2 className="ml-4 font-bold">First Name</h2>
         <input
           type="text"
-          placeholder="enter title of your story"
-          value={user.firstName}
+          placeholder="enter your first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
           className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
         />
         <h2 className="ml-4 font-bold">Last Name</h2>
         <input
           type="text"
-          placeholder="enter number of pages"
-          value={user.lastName}
+          placeholder="enter your last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
           className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
         />
         <h2 className="ml-4 font-bold">Email</h2>
         <input
           type="text"
-          placeholder="enter number of pages"
-          value={user.email}
+          placeholder="enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
+        />
+        <h2 className="ml-4 font-bold">Password</h2>
+        <input
+          type="password"
+          placeholder="enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
+        />
+        <h2 className="ml-4 font-bold">Confirm Password</h2>
+        <input
+          type="password"
+          placeholder="confirm your password"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
           className="border-2 border-gray-200 p-2 rounded-md mx-4 mb-4 text-gray-400"
         />
         <div className="flex items-center justify-center gap-6">
